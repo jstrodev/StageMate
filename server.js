c51b1c2c777f6813824853690ff0c237aff7a2b4
jstrodev <jcastrodev1836@gmail.com>
2025-03-03 15:16:35 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -351,10 +351,12 @@ app.get("/api/test", async (req, res) => {
 
 // Serve static files in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  // Serve static files from the React frontend app
+  app.use(express.static(path.join(__dirname, "client/dist")));
 
+  // Handle React routing, return all requests to React app
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/dist", "index.html"));
   });
 }
 
